b8ad85674f8b6451db8a8a0fda334fbd4270f8d1
Michael Carmody <mr.carmody89@gmail.com>
2021-07-26 10:11:40 -0400

    Updated pages

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var musicsearch = document.querySelector('#musicsearch');
 var Musicsearchitem = document.querySelector('#Music-Search-Item')
   
 function MusicSearch(query) {
-    var locMMURL = `https://api.musixmatch.com/ws/1.1/track.search?apikey=54136b8efe820fb6dfbc6d2a8179c359&q=${query}`
-    fetch(locMMURL)
+    var MMURL = `https://api.musixmatch.com/ws/1.1/track.search?apikey=54136b8efe820fb6dfbc6d2a8179c359&q=${query}`
+    fetch(MMURL)
     .then(function (response) {
       if(response.ok) {
         response.json().then(function (data) {
@@ -70,31 +70,71 @@ function printResults(Music, searchterm) {
         }
         
         var linkButton = document.createElement('a');
-        linkButton.textContent = 'Listen Here';
-        linkButton.onclick = function () {YTResults};
+        linkButton.textContent = 'Dive Deeper';
+        //linkButton.onclick = YTResults();
         linkButton.classList.add('btn',);
         
         resultBody.append(linkButton, bodyContentEl);
-        //resultBody.appendChild(bodyContentEl);
         resultContent.append(resultCard);
     }
 }
 
-function YTResults() {
-    fetch("https://deezerdevs-deezer.p.rapidapi.com/track/%7Bid%7D", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "4b9eb06844msh2593a5e43d37842p15d2e7jsnd7dac4b64d82",
-            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
-        }
-    })
-    .then(response => {
-        console.log(response);
-    })
-    .catch(err => {
-        console.error(err);
-    });
-}
+// function ShowResults(Second, Deeper) {
+   
+//     Musicsearchitem.textContent = Deeper;
+
+//     for (var i=0; i < Second.length; i++) {
+//         console.log(Second);
+                
+//         var resultCard = document.createElement('div');
+//         resultCard.classList.add('card', 'blue-grey', 'darken-1');
+        
+//         var resultBody = document.createElement('div');
+//         resultBody.classList.add('card-body');
+//         resultCard.append(resultBody);
+        
+        
+//         var bodyContentEl = document.createElement('p');
+//         bodyContentEl.innerHTML =
+//             '<strong>Album Name:</strong> ' + Second[i].track.album_name + '<br/>';
+                
+//         if (Second.message.body.track_list[i].track.artist_name) {
+//             bodyContentEl.innerHTML +=
+//             '<strong>Artist Name:</strong> ' + Second.message.body.track_list[i].track.artist_name + '<br/>';
+//         } else {
+//             bodyContentEl.innerHTML +=
+//             '<strong>Artist Name:</strong> No subject for this entry.' + '<br/>';
+//         }
+        
+//         if (Second.message.body.track_list[i].track.track_name) {
+//             bodyContentEl.innerHTML +=
+//             '<strong>Track:</strong> ' + Second.message.body.track_list[i].track.track_name;
+//         } else {
+//             bodyContentEl.innerHTML +=
+//             '<strong>Track:</strong>  No description for this entry.';
+//         }
+        
+//         resultBody.append(bodyContentEl);
+//         resultContent.append(resultCard);
+//     }
+// }
+
+// function YTResults(track_name) {
+//    var Ufinder = `https://www.googleapis.com/youtube/v3/videos?id=71CDEYXw3mM&key=AIzaSyCMOEOHziR1Ycifilh5J3nJ5S7RqC4VzSo&q=${bodyContentEl.value}`
+//    fetch(Ufinder)
+//    .then(function (response) {
+//        if(response.ok) {
+//            response.json().then(function (data) {
+//                ShowResults (data.items, track_name)
+//            });
+//        } else {
+//            return response.json();
+//        }
+//    })
+//    .catch(function (error) {
+//        console.error(error);
+//    });
+// };
 
 
 
